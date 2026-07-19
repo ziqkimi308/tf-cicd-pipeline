00bf8e2ccbee21143fe72ff70eec508f29f5a8f0
Haziq Hakimi <haziqhakimiyes@gmail.com>
2026-07-19 11:13:25 +0800

    chore: add vpc module files

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
