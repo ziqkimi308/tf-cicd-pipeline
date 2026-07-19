d96e08729c7e11724c988f9bdcb9bbff3e95199e
Haziq Hakimi <haziqhakimiyes@gmail.com>
2026-07-19 17:45:32 +0800

    chore: changed back from t2 to t3.micro

--- variables.tf
+++ variables.tf
@@ -25,7 +25,7 @@ variable "public_subnet_cidr" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "ami_id" {
